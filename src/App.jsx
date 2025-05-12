44e4ee84ee15d76bc636fe877bf8f7c31ca16b38
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-05-12 19:51:10 +0500

    events

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css'
-import Home from './Components/Home'
+import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
-
+  // const risk = {
+  //   no: 1234,
+  //   color: "Black",
+  // };
 
   return (
     <>
       <h1>Hello</h1>
-      <Home Pages={100}
-      name={{name1:"Ghulam Shabbir",
-        des:"Graduate"
-      }}
+      <Home
+        Pages={100}
+        name={{ name1: "Ghulam Shabbir", des: "Graduate", risk }
+      }
       />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
 function Home({Page,name}) {
+  const handleClick=()=>{
+    alert("Click me")
+  }
+  const shoot = (a) => {
+    alert(a);
+  }
+
+  const raw=(b)=>{
+    alert(b)
+  }
   return (
     <>
       <h1>This is Home</h1>
       <h2>{Page}</h2>
       <p>The person name is {name.name1} and he is {name.des}</p>
+      {/* <p>The person number is {risk.no} and his color is {risk.color}</p> */}
 
+
+<button type="button" onClick={handleClick}>Click me</button>
+<button onClick={() => shoot("Goal!")}>Take the shot!</button>
+<button type="button" onClick={()=>{
+  raw(7)
+}}>raw</button>
     </>
   )
 }
