f4b2d798ef73090e24593fe1502f9a7b30fbde85
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-05-16 12:19:21 +0500

    unknown

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import "./App.css";
 import Home from "./Components/Home";
 
 function App() {
-  // const risk = {
-  //   no: 1234,
-  //   color: "Black",
-  // };
-
   return (
     <>
       <h1>Hello</h1>
       <Home
         Pages={100}
-        name={{ name1: "Ghulam Shabbir", des: "Graduate", risk }
-      }
+        name={{ name1: "Ghulam Shabbir", des: "Graduate" }}
+        risk={{
+          no: 1234,
+          color: "Black",
+        }}
       />
     </>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React from "react";
 
-function Home({Page,name}) {
-  const handleClick=()=>{
-    alert("Click me")
-  }
+function Home({ Page, name, risk }) {
+  const handleClick = () => {
+    alert("Click me");
+  };
   const shoot = (a) => {
     alert(a);
-  }
+  };
 
-  const raw=(b)=>{
-    alert(b)
-  }
+  const raw = (b) => {
+    alert(b);
+  };
   return (
     <>
       <h1>This is Home</h1>
       <h2>{Page}</h2>
-      <p>The person name is {name.name1} and he is {name.des}</p>
-      {/* <p>The person number is {risk.no} and his color is {risk.color}</p> */}
+      <p>
+        The person name is {name.name1} and he is {name.des}
+      </p>
+      <p>
+        The person number is {risk.no} and his color is {risk.color}
+      </p>
 
-
-<button type="button" onClick={handleClick}>Click me</button>
-<button onClick={() => shoot("Goal!")}>Take the shot!</button>
-<button type="button" onClick={()=>{
-  raw(7)
-}}>raw</button>
+      <button type="button" onClick={handleClick}>
+        Click me
+      </button>
+      <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+      <button
+        type="button"
+        onClick={() => {
+          raw(7);
+        }}
+      >
+        raw
+      </button>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
